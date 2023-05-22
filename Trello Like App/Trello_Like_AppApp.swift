4c059c4f4f4558163ad0cf07eb051bd818591474
bryangwin <bryangwin@gmail.com>
2023-05-22 12:57:18 -0700

    Initial Commit

---
+++ Trello Like App/Trello_Like_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trello_Like_AppApp.swift
+//  Trello Like App
+//
+//  Created by Bryan Gwin on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Trello_Like_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
